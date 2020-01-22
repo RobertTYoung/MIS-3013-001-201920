cd55058641b7ec314080e29ca5332faa5039dc47
RobertTYoung <youngrobert4321@gmail.com>
2020-01-22 12:55:07 -0600

    Created variables for firstName, lastName, favoriteNumber, crazyNumber, and fullName.

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -11,7 +11,26 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create string named FirstName
+            string firstName;
 
+            //Assign a value to firstName (use your name)
+            firstName = "Robert";
+
+            //Create an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 7;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Young";
+            string fullName = firstName + " " + lastName;
+            //string fullName = ($"{firstName}{lastName}");
+
+            Console.Write(fullName + " has the favorite number of " + favoriteNumber + " \nwhich equates to the crazy number " + crazyNumber);
+
+            //MAKE IT DYNAMIC
+
+            Console.ReadKey();
         }
     }
 }
