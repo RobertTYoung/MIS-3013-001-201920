e5e66c0ecb81a48b64946a0367628e634a49f434
RobertTYoung <youngrobert4321@gmail.com>
2020-01-22 12:35:17 -0600

    Created the in classs examples soultions and created a project for variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿//Robert Young
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
