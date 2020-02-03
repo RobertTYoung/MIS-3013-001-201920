2723a62ea7864925147e265818a8d8a049585348
RobertTYoung <youngrobert4321@gmail.com>
2020-02-03 12:44:37 -0600

    Added Loop

--- (P) Conditional - Coin Toss/(P) Conditional - Coin Toss/Program.cs
+++ (P) Conditional - Coin Toss/(P) Conditional - Coin Toss/Program.cs
@@ -11,45 +11,100 @@ namespace _P__Conditional___Coin_Toss
         const string MESSAGE = ("Developed by Robert Young.");
         static void Main(string[] args)
         {
-            //int Heads = 0;
-            //int Tails = 1;
-
-            Console.WriteLine( "Enter Heads or Tails >>");
-            string usersGuessAsString = Console.ReadLine();
-            int usersGuess;
-            const int HEADS_VALUE = 0;
-            const int TAILS_VALUE = 1;
-           
-            if (usersGuessAsString == "Heads")
-            {
-                usersGuess = HEADS_VALUE;
-            }
-            else
-            {
-                usersGuess = TAILS_VALUE;
-            }
-
+            int randomNumber ;
+            int usersGuess ;
             Random rand = new Random();
-            //use the nest method on 'rand' and pass in 0,2 to generate a random number taht is 0 or 1
-            int randomNumber = rand.Next(0, 2);
- 
-            if (usersGuess == randomNumber)
-            {
-                Console.WriteLine("You are right!");
-            }
-            else
-            {
-                Console.WriteLine("You are Wrong!");
-            }
+            int numberOfGuessses = 0;
 
-            if (usersGuessAsString == "Heads")
+            /*while (usersGuess != randomNumber)
             {
-                Console.WriteLine("It landed on" + usersGuess);
-            }
-            else
+                randomNumber = rand.Next(0, 2);
+                Console.WriteLine(randomNumber);
+
+                Console.WriteLine("Enter Heads or Tails >>");
+                string usersGuessAsString = Console.ReadLine();
+                
+                const int HEADS_VALUE = 0;
+                const int TAILS_VALUE = 1;
+
+                if (usersGuessAsString == "Heads")
+                {
+                    usersGuess = HEADS_VALUE;
+                }
+                else
+                {
+                    usersGuess = TAILS_VALUE;
+                }/
+
+              
+                //use the nest method on 'rand' and pass in 0,2 to generate a random number taht is 0 or 1
+               
+
+                if (usersGuess == randomNumber)
+                {
+                    Console.WriteLine("You are right!");
+                }
+                else
+                {
+                    Console.WriteLine("You are Wrong!");
+                }
+
+                if (usersGuessAsString == "Heads")
+                {
+                    Console.WriteLine("It landed on" + usersGuess);
+                }
+                else
+                {
+                    Console.WriteLine("It didn't land on " + usersGuess);
+                }
+            }*/
+
+            do
             {
-                Console.WriteLine("It didn't land on " + usersGuess);
-            }
+                {
+                    randomNumber = rand.Next(0, 2);
+                    Console.WriteLine(randomNumber);
+
+                    Console.WriteLine("Enter Heads or Tails >>");
+                    string usersGuessAsString = Console.ReadLine();
+
+                    const int HEADS_VALUE = 0;
+                    const int TAILS_VALUE = 1;
+
+                    if (usersGuessAsString == "Heads")
+                    {
+                        usersGuess = HEADS_VALUE;
+                    }
+                    else
+                    {
+                        usersGuess = TAILS_VALUE;
+                    }
+
+
+                //use the nest method on 'rand' and pass in 0,2 to generate a random number taht is 0 or 1
+
+
+                    if (usersGuess == randomNumber)
+                    {
+                        Console.WriteLine("You are right!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are Wrong!");
+                    }
+
+                    if (usersGuessAsString == "Heads")
+                    {
+                        Console.WriteLine("It landed on" + usersGuess);
+                    }
+                    else
+                    {
+                        Console.WriteLine("It didn't land on " + usersGuess);
+                    }
+                }
+                numberOfGuessses = numberOfGuessses + 1;
+            } while (usersGuess != randomNumber);
+            Console.WriteLine($"It took you { numberOfGuessses.ToString("N0")} to guess right");
 
             Console.WriteLine(MESSAGE);
             Console.ReadKey(); //Prevent teh application from closing until we press a key
