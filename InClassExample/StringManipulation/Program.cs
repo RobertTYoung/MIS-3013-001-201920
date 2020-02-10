03f5b0621c644d0fba94a78a718229c7cda03c44
RobertTYoung <youngrobert4321@gmail.com>
2020-02-10 12:54:50 -0600

    Name flipping and trim

--- InClassExample/StringManipulation/Program.cs
+++ InClassExample/StringManipulation/Program.cs
@@ -12,7 +12,60 @@ namespace StringManipulation
         {
             //                  0123
             string FirstName = "Robert";
-            String LastName = "Young"; 
+            String LastName = "Young";
+
+            Console.WriteLine($"You have {FirstName.Length.ToString("N0")} characters in your first name.");
+            //Console.WriteLine("You have " + FirstName.Length.ToString("N0") + "characters in your first name.");
+            Console.WriteLine($"You have {LastName.Length.ToString("N0")} characters in your last name.");
+            char FirstInitial = FirstName[0];
+            char LastInitial = LastName[0];
+
+            /* Console.WriteLine(FirstName.ToUpper()[0]);
+            Console.WriteLine(FirstName.ToUpper()[1]);
+            Console.WriteLine(FirstName.ToUpper()[2]);
+            Console.WriteLine(FirstName.ToUpper()[3]);
+            Console.WriteLine(FirstName.ToUpper()[4]);
+            Console.WriteLine(FirstName.ToUpper()[5]); */
+
+            FirstName = FirstName.ToUpper();
+            //output every charater of the firstname on a sepertate line in BIG LETTERS
+            /* for (int i = 0; i < FirstName.Length; i++)
+             {
+                 char currentLetter = FirstName[i];
+                 Console.WriteLine(currentLetter);
+             } */
+
+          
+            foreach (var currentLetter in FirstName)
+            {
+                Console.WriteLine(currentLetter);
+            }
+
+            //Robert Young
+            //gnuoY treboR
+
+            string Name = FirstName + " " + LastName;
+            var pieces = Name.Split(' ');
+            string fName = Name.Substring(0, FirstName.Length);
+            fName = "      " + fName + "     ";
+            fName = fName.Trim();
+
+            for (int i = Name.Length - 1; i >= 0; i--)
+            {
+                Console.Write(Name[i]);
+            }
+
+          
+            Console.WriteLine("Output every other letter of the firstname");
+            for (int i = 0; i < FirstName.Length; i = i + 2)
+            {
+                char currentLetter = FirstName[i];
+                Console.WriteLine(currentLetter + " ");
+            }
+
+            Console.WriteLine($"Your initails are {FirstInitial}.{LastInitial}.");
+
+            Console.ReadKey();
 
         }
     }
