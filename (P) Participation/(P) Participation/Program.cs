30dc22aff4cfb5ce8c479530f996ac82a3b87b62
RobertTYoung <youngrobert4321@gmail.com>
2020-02-19 12:45:17 -0600

    First save

--- (P) Participation/(P) Participation/Program.cs
+++ (P) Participation/(P) Participation/Program.cs
@@ -7,9 +7,25 @@ using System.Threading.Tasks;
 namespace _P__Collections
 {
     class Program
-    {//Robert Young
+    {//Robert Young collaberted with Shaun Poweshiek
         static void Main(string[] args)
-        {
+        {                      
+            
+            Console.WriteLine("Please enter an exam grade! >>");
+            double eGrade = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Do you have another exam score? Please enter true or false. >>");
+            bool Another = Convert.ToBoolean(Console.ReadLine());
+
+            List<double> examGrades = new List<double>();
+            examGrades.Add(eGrade);
+
+            do
+            {
+                examGrades.Add(eGrade);
+            } while (Another == true);
+            
+
+            Console.ReadKey();
         }
     }
 }
