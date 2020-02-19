c8b0d6cf35b6daf1da0afbced3a9559320f01177
RobertTYoung <youngrobert4321@gmail.com>
2020-02-19 13:13:09 -0600

    almost completed input

--- (P) Participation/(P) Participation/Program.cs
+++ (P) Participation/(P) Participation/Program.cs
@@ -9,21 +9,31 @@ namespace _P__Collections
     class Program
     {//Robert Young collaberted with Shaun Poweshiek
         static void Main(string[] args)
-        {                      
-            
-            Console.WriteLine("Please enter an exam grade! >>");
-            double eGrade = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Do you have another exam score? Please enter true or false. >>");
-            bool Another = Convert.ToBoolean(Console.ReadLine());
+        {
+
+            //Console.WriteLine("Please enter an exam grade! >>");
+            //double eGrade = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Do you have another exam score? Please enter true or false. >>");
+            //bool Another = Convert.ToBoolean(Console.ReadLine());
+            //double eGrade = 0;
+            bool Another = true;
 
             List<double> examGrades = new List<double>();
-            examGrades.Add(eGrade);
+            //examGrades.Add(eGrade);
 
             do
             {
+                Console.WriteLine("Please enter an exam grade! >>");
+                double eGrade = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Do you have another exam score? Please enter true or false. >>");
+                bool AnotherOne = Convert.ToBoolean(Console.ReadLine());
+               
                 examGrades.Add(eGrade);
+
             } while (Another == true);
-            
+
+            int results = examGrades.Count();
+            Console.WriteLine($"You've entered {results} grade(s)! >>" );
 
             Console.ReadKey();
         }
