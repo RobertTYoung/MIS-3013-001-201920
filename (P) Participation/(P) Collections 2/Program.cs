35441dd86c301b8e37f738ad38aa98c273295ef1
RobertTYoung <youngrobert4321@gmail.com>
2020-03-02 13:04:11 -0600

    progress on part two

---
+++ (P) Participation/(P) Collections 2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P__Collections_2
+{
+    class Program
+    {//Robert Young
+        static void Main(string[] args)
+        {
+            //1
+            //string [] classes = { "MIS", "MATH", "MGT", "FIN" };
+            //string[] courseNum = { "3013", "3033", "3013", "2303" };
+
+            string[] classes = new string[3];
+            string[] courseNum = new string[3];
+
+            for (int i = 0; i < classes.Length; i++)
+            {
+                Console.WriteLine("Please enter your course name. >>");
+                classes[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < courseNum.Length; i++)
+            {
+                Console.WriteLine("Please enter your course number. >>");
+                courseNum[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < classes.Length; i++)
+            {
+                Console.WriteLine(string.Concat(classes[i].ToUpper(), courseNum[i].ToUpper()));
+            }
+            //Console.WriteLine(string.Concat(classes[0], courseNum[0]));
+            //Console.WriteLine("Please enter the number 3.");
+
+            //2
+            List<double> grades = new List<double>();
+            string assignmentScore, answer;
+            double sum = 0;
+
+            do
+            {
+                Console.WriteLine($"Please enter your grade.");
+                assignmentScore = Console.ReadLine();
+                double gradeScore = Convert.ToDouble(assignmentScore);
+                grades.Add(gradeScore);
+
+                Console.WriteLine("Do you have another exam score? Please enter yes or no. >>");
+                answer = Console.ReadLine();
+            } while (answer.ToLower() == "yes");
+
+            //double sum = 0
+            double average = sum / assignmentScore.Count;
+
+            Console.WriteLine($"Your average for the {assignmentScore.Count} grades is {average.ToString("N3")}");
+
+            //3
+
+            Console.ReadKey();
+        }
+    }
+}
