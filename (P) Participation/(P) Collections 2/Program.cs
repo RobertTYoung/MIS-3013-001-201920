39d874fe48de702f3d211c3a76bb283f824c2065
RobertTYoung <youngrobert4321@gmail.com>
2020-03-02 13:12:15 -0600

    finished second part

--- (P) Participation/(P) Collections 2/Program.cs
+++ (P) Participation/(P) Collections 2/Program.cs
@@ -14,7 +14,7 @@ namespace _P__Collections_2
             //string [] classes = { "MIS", "MATH", "MGT", "FIN" };
             //string[] courseNum = { "3013", "3033", "3013", "2303" };
 
-            string[] classes = new string[3];
+            /*string[] classes = new string[3];
             string[] courseNum = new string[3];
 
             for (int i = 0; i < classes.Length; i++)
@@ -32,30 +32,37 @@ namespace _P__Collections_2
             for (int i = 0; i < classes.Length; i++)
             {
                 Console.WriteLine(string.Concat(classes[i].ToUpper(), courseNum[i].ToUpper()));
-            }
+            }*/
             //Console.WriteLine(string.Concat(classes[0], courseNum[0]));
             //Console.WriteLine("Please enter the number 3.");
 
             //2
             List<double> grades = new List<double>();
-            string assignmentScore, answer;
-            double sum = 0;
+            string assignmentScore, answer;            
+            double gradeScore;
 
             do
             {
                 Console.WriteLine($"Please enter your grade.");
                 assignmentScore = Console.ReadLine();
-                double gradeScore = Convert.ToDouble(assignmentScore);
+                gradeScore = Convert.ToDouble(assignmentScore);
                 grades.Add(gradeScore);
 
                 Console.WriteLine("Do you have another exam score? Please enter yes or no. >>");
                 answer = Console.ReadLine();
             } while (answer.ToLower() == "yes");
 
-            //double sum = 0
-            double average = sum / assignmentScore.Count;
+            double sum = 0;
 
-            Console.WriteLine($"Your average for the {assignmentScore.Count} grades is {average.ToString("N3")}");
+            for (int i = 0; i < grades.Count; i++)
+            {
+                double score = grades[i];
+                sum = sum + score;
+            }
+                       
+            double average = sum / grades.Count;
+
+            Console.WriteLine($"Your average for the {grades.Count} grades is {average.ToString("N3")}");
 
             //3
 
