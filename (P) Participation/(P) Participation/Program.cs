f3b224fa61d85533a44fee5972e9c10e3de335e1
RobertTYoung <youngrobert4321@gmail.com>
2020-03-02 12:22:55 -0600

    updated

--- (P) Participation/(P) Participation/Program.cs
+++ (P) Participation/(P) Participation/Program.cs
@@ -9,8 +9,8 @@ namespace _P__Collections
     class Program
     {//Robert Young collaberted with Shaun Poweshiek
         static void Main(string[] args)
-        {            
-            bool Another = true;
+        {
+            /*bool Another = true;
 
             List<double> examGrades = new List<double>();
             
@@ -26,7 +26,63 @@ namespace _P__Collections
             } while (Another == true);
 
             int results = examGrades.Count();
-            Console.WriteLine($"You've entered {results} grade(s)! >>" );
+            Console.WriteLine($"You've entered {results} grade(s)! >>" );*/
+
+            string examScore, answer;
+            List<double> scores = new List< double > ();
+            
+            do
+            {
+                Console.WriteLine($"Please enter your exam score.");
+                examScore = Console.ReadLine();
+                double es = Convert.ToDouble(examScore);
+
+                Console.WriteLine("Do you have another exam score? Please enter yes or no. >>");
+                answer = Console.ReadLine();
+            } while (answer.ToLower() == "yes");
+
+            double sum = 0;
+            double minimum = 100;
+            double maximum = -1;
+
+            /*for (int i = 0; i < scores.Count; i++)
+            {
+                double score = scores[i];
+                sum = sum + score;
+
+                if (score < minimum)
+                {
+                    minimum = score;
+                }
+
+                if (score > maximum)
+                {
+                    maximum = score;
+                }
+            }*/
+
+            foreach (var score in scores)
+            {
+                sum = sum + score;
+
+                if (score < minimum)
+                {
+                    minimum = score;
+                }
+
+                if (score > maximum)
+                {
+                    maximum = score;
+                }
+            }
+
+
+            double average = sum / scores.Count;
+
+            Console.WriteLine($"Your average for the {scores.Count} exam grade is {average.ToString("N3")}");
+            Console.WriteLine($"Your minimum score for the exam grade is {minimum.ToString("N3")}");
+            Console.WriteLine($"Your max score for the exam grade is {maximum.ToString("N3")}");
+
 
             Console.ReadKey();
         }
