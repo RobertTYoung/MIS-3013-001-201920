6a418e3abe4966feadf6e349de47ff7be80c07b0
RobertTYoung <youngrobert4321@gmail.com>
2020-03-09 13:12:03 -0500

    fianl save

--- (P) Participation/(P) Functions - Calculator/Program.cs
+++ (P) Participation/(P) Functions - Calculator/Program.cs
@@ -8,46 +8,69 @@ namespace _P__Functions___Calculator
 {
     class Program
     {//Robert Young
+     //Ask the user if they would like to perform a new calculation, or use the result of their previous calculation as the first value in a follow-up calculation, or if they will wish to stop.
 
-       
         static void Main(string[] args)
-        {     
-            string DeveloperName = " ";
-            string ClassName = " ";
-            string Date = " ";
+        {
+            string Continue = "Yes";
+            while (Continue == "Yes")
+            {           
+                string DeveloperName = " ";
+                string ClassName = " ";
+                string Date = " ";
 
-            string InfoMessage = DeveloperInformation(DeveloperName, ClassName, Date);
-            Console.WriteLine(InfoMessage);
+                string InfoMessage = DeveloperInformation(DeveloperName, ClassName, Date);
+                Console.WriteLine(InfoMessage);
 
-            Console.WriteLine("What kind of calculation would you like to do? Add, Substract, Multiply, or Divide >>");
-            string FunctionType = Console.ReadLine();
+                Console.WriteLine("What kind of calculation would you like to do? Add, Substract, Multiply, or Divide >>");
+                string FunctionType = Console.ReadLine();
 
-            Console.WriteLine("What is your first number?");
-            double firstNum = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("What is your second number?");
-            double secondNum = Convert.ToDouble(Console.ReadLine());
-            //double Answer = FunctionType(firstNum, secondNum);
-            double result = 0;
+                Console.WriteLine("What is your first number?");
+                double firstNum = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("What is your second number?");
+                double secondNum = Convert.ToDouble(Console.ReadLine());
+                //double Answer = FunctionType(firstNum, secondNum);
+                double result = 0;
 
-            if (FunctionType == "Add")
-            {
-                result = Add(firstNum, secondNum);
-                Console.WriteLine(result);
-            }
-            else if (FunctionType == "Subtract")
-            {
-                result = Subtract(firstNum, secondNum);
-                Console.WriteLine(result);
-            }
-            else if (FunctionType == "Multiply")
-            {
-                result = Multiply(firstNum, secondNum);
-                Console.WriteLine(result);
-            }
-            else if (FunctionType == "Divide")
-            {
-                result = Divide(firstNum, secondNum);
-                Console.WriteLine(result);
+                if (FunctionType == "Add")
+                {
+                    result = Add(firstNum, secondNum);
+                    Console.WriteLine(result);
+                }
+                else if (FunctionType == "Subtract")
+                {
+                    result = Subtract(firstNum, secondNum);
+                    Console.WriteLine(result);
+                }
+                else if (FunctionType == "Multiply")
+                {
+                    result = Multiply(firstNum, secondNum);
+                    Console.WriteLine(result);
+                }
+                else if (FunctionType == "Divide")
+                {
+                    result = Divide(firstNum, secondNum);
+                    Console.WriteLine(result);
+                }
+
+                Console.WriteLine("Would you like to make another calculation? YES or NO >>");
+                Continue = Console.ReadLine().ToUpper();
+
+                if (Continue == "Yes")
+                {
+                    Console.WriteLine("Would you like to use you answer as first number in the next calculation? YES Or NO >>");
+                    string AnswerAsFirstNum = Console.ReadLine().ToUpper();
+                }
+                //Console.WriteLine("Would you like to use you answer as first number in the next calculation? YES Or NO >>");
+                //string AnswerAsFirstNum = Console.ReadLine().ToUpper();
+                else
+                {
+                    Console.WriteLine("Would you like to stop? YES Or NO >>");
+                    string Stop = Console.ReadLine().ToUpper();
+                } 
+                //Console.WriteLine("Would you like to stop? YES Or NO >>");
+                //string Stop = Console.ReadLine().ToUpper();
+
             }
 
             Console.ReadKey();
