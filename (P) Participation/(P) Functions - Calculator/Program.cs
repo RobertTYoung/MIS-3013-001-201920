b6a963927577f02c7882bab1107fd7030808eced
RobertTYoung <youngrobert4321@gmail.com>
2020-03-09 12:15:22 -0500

    Created all of the functions for the functions participation

---
+++ (P) Participation/(P) Functions - Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P__Functions___Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.ReadKey();
+        }
+        static int Add(int first, int second)
+        {
+            return first + second;
+        }
+        static double Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        static int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+        static int Divide(int first, int second)
+        {
+            return first / second;
+        }
+        static string DeveloperInformation(string DeveloperName, string ClassName, string Date)
+        {
+            return ($"{DeveloperName} created this application for {ClassName} on {Date}.");
+        }
+        //developers name, a string for the class name, and a string for the date of writing ,, who wrote the application, for what class, and on what date
+    }
+}
